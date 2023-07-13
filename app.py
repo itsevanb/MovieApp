f9cdea960bbdfe36c5059c8d511389e5e15e5c65
Evan <116227653+itsevanb@users.noreply.github.com>
2023-07-13 02:37:06 -0500

    login/logout/register, works successfully

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, session
 from movie_web_app.datamanager.json_data_manager import JSONDataManager
 import requests
+from werkzeug.security import check_password_hash
+import os
+
+"""Generate a Secret Key for the session."""
+secret_key = os.urandom(24).hex()
 
 app = Flask(__name__)
+app.secret_key = secret_key
 data_manager = JSONDataManager('movies.json')
+
 
 
 @app.route('/')
@@ -15,6 +22,8 @@ def home():
 @app.route('/users/')
 def users_list():
     """Render a list of all users registered in the movie app."""
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     users = data_manager.get_all_users()
     return render_template('users.html', users=users)
 
@@ -29,6 +38,8 @@ def user_movies(user_id):
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
     """Handle the form for adding a new user."""
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     if request.method == 'POST':
         name = request.form['name']
         try:
@@ -43,6 +54,8 @@ def add_user():
 @app.route('/add_movie', methods=['GET', 'POST'])
 def add_movie():
     """Handle the form for adding a new movie to a user's favorites. Pull data from the OMDB API."""
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     if request.method == 'POST':
         user_id = int(request.form['user_id'])
         title = request.form['title'].strip()
@@ -71,6 +84,8 @@ def add_movie():
 @app.route('/update_movie', methods=['GET', 'POST'])
 def update_movie():
     """Handle the form for updating a movie in a user's favorites."""
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     if request.method == 'POST':
         user_id = int(request.form['user_id'])
         movie_id = int(request.form['movie_id'])
@@ -89,6 +104,8 @@ def update_movie():
 @app.route('/delete_movie', methods=['GET', 'POST'])
 def delete_movie():
     """Handle the form for deleting a movie from a user's favorites."""
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     if request.method == 'POST':
         user_id = int(request.form['user_id'])
         movie_id = request.form['movie_id']
@@ -100,6 +117,41 @@ def delete_movie():
             return render_template('error.html', error_message=str(e))
     return render_template('delete_movie.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Handle the form for registering a new user."""
+    if request.method == 'POST':
+        name = request.form['name']
+        username = request.form['username']
+        password = request.form['password']
+        try:
+            data_manager.add_user(name, username, password)
+            return redirect(url_for('login'))
+        except Exception as e:
+            print(f"An error occurred while adding a new user: {str(e)}")
+            return render_template('error.html', error_message=str(e))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Handle the form for logging in a user."""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        users = data_manager.get_all_users()
+        user = next((user for user in users if 'username' in user and user['username'] == username), None)
+        if user and check_password_hash(user['password'], password):
+            session['user_id'] = user['id']
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error_message='Incorrect username or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    """Handle the form for logging out a user."""
+    session.pop('user_id', None)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
